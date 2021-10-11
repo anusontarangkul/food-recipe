d07b226cfa781dd1382be8369a9573a5fe6ad4c8
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-10-11 15:35:32 -0700

    add fallback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const staticCacheName = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -10,7 +10,8 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v109/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v109/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
 ]
 
 self.addEventListener('install', evt => {
@@ -28,7 +29,7 @@ self.addEventListener('activate', evt => {
         caches.keys().then(keys => {
             console.log(keys)//
             return Promise.all(keys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
                 .map(key => caches.delete(key))
             )
         })
@@ -40,11 +41,11 @@ self.addEventListener('fetch', evt => {
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
             return cacheRes || fetch(evt.request).then(fetchRes => {
-                return caches.open(dynamicCache).then(cache => {
+                return caches.open(dynamicCacheName).then(cache => {
                     cache.put(evt.request.url, fetchRes.clone());
                     return fetchRes;
                 })
             })
-        })
+        }).catch(() => caches.match('/pages/fallback.html'))
     )
 })
